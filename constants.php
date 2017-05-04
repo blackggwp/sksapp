e8991b51ed6aad2cb6ef5bcdc8114fe8a133de76
blackMBP <you@example.com>
2017-05-04 15:57:48 +0700

    Upload necessary file

---
+++ constants.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'swift_db');
+
+define('USER_CREATED', 0);
+define('USER_ALREADY_EXIST', 1);
+define('USER_NOT_CREATED', 2);
+?>
---
+++ dbopr.php
@@ -0,0 +1,79 @@
+<?php
+
+class DbOperation
+{
+    private $conn;
+
+    //Constructor
+    function __construct()
+    {
+        require_once dirname(__FILE__) . '/constants.php';
+        require_once dirname(__FILE__) . '/dbconnect.php';
+        // opening db connection
+        $db = new DbConnect();
+        $this->conn = $db->connect();
+    }
+
+    //Function to create a new user
+//     public function createUser($username, $pass, $email, $name, $phone)
+//     {
+//         if (!$this->isUserExist($username, $email, $phone)) {
+//             $password = md5($pass);
+//             $stmt = $this->conn->prepare("INSERT INTO users (username, password, email, name, phone) VALUES (?, ?, ?, ?, ?)");
+//             $stmt->bind_param("sssss", $username, $password, $email, $name, $phone);
+//             if ($stmt->execute()) {
+//                 return USER_CREATED;
+//             } else {
+//                 return USER_NOT_CREATED;
+//             }
+//         } else {
+//             return USER_ALREADY_EXIST;
+//         }
+//     }
+
+
+//     private function isUserExist($username, $email, $phone)
+//     {
+//         $stmt = $this->conn->prepare("SELECT id FROM users WHERE username = ? OR email = ? OR phone = ?");
+//         $stmt->bind_param("sss", $username, $email, $phone);
+//         $stmt->execute();
+//         $stmt->store_result();
+//         return $stmt->num_rows > 0;
+//     }
+// }
+
+    //Function to create a new user
+    public function createUser($username, $pass, $email, $phone)
+    {
+        if (!$this->isUserExist($email)) {
+            $password = md5($pass);
+            $stmt = $this->conn->prepare("INSERT INTO users (username, password, email, phone) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("ssss", $username, $password, $email, $phone);
+            if ($stmt->execute()) {
+                return USER_CREATED;
+            } else {
+                return USER_NOT_CREATED;
+            }
+        } else {
+            return USER_ALREADY_EXIST;
+        }
+    }
+
+
+    private function isUserExist($email)
+    {
+        $stmt = $this->conn->prepare("SELECT id FROM users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
+        return $stmt->num_rows > 0;
+    }
+    private function loginUser($email,$pass)
+    {
+        $stmt = $this->conn->prepare("SELECT id FROM users WHERE email = ? and pass = ?");
+        $stmt->bind_param("ss", $email,$pass);
+        $stmt->execute();
+        $stmt->store_result();
+        return $stmt->num_rows > 0;
+    }
+}
